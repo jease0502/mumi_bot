6c670478144fb56873bfad25574c62df0d884654
jease0502 <jease0502@gmail.com>
2022-04-14 17:57:49 +0800

    fixed db qusetion

--- mongo/mongo.py
+++ mongo/mongo.py
@@ -6,8 +6,8 @@ class MongoDB (object):
         self.data = self._init_get_data()
 
     def _init_get_data(self):
-        mydb = self.myclient["mumi_msg"]
-        collection = mydb["msg"].find_one()
+        self.mydb = self.myclient["mumi_msg"]["msg"]
+        collection = self.mydb["msg"].find_one()
         return collection
 
     def get_data(self , msg):
@@ -18,21 +18,21 @@ class MongoDB (object):
             return False
 
     def insert_data(self, answer , question):
-        self.data.insert_one({answer : question})
+        self.mydb.insert_one({answer : question})
         if(self.get_data(answer) == question):
             return True
         else:
             return False
 
     def delete_data(self, answer , question):
-        self.data.delete_one({answer : question})
+        self.mydb.delete_one({answer : question})
         if(self.get_data(answer) == None):
             return True
         else:
             return False
 
     def update_data(self, answer , question):
-        self.data.update_one({answer : question})
+        self.mydb.update_one({answer : question})
         if(self.get_data(answer) == question):
             return True
         else:
